37edbae921f7951072dc7b45c4498a065c727925
Sagar Dhunna <sagardhunna17@gmail.com>
2024-12-21 01:45:34 -0800

    testing

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // This ensures that routing works correctly on the deployed site
+  build: {
+    outDir: 'dist', // Ensures that the build output goes to the dist directory
+  },
 })
