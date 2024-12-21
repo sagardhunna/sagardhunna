c89e1bbd6fe97cdd2de0535e43a10c730c808c3c
Sagar Dhunna <sagardhunna17@gmail.com>
2024-12-21 01:40:18 -0800

    back to ground 0

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ensures app works at the root URL
-  build: {
-    outDir: 'dist', // Ensures Vite outputs to the dist folder
-  },
 })
