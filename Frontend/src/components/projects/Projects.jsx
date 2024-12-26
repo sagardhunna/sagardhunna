afc6c12781201e7eeff7061bc08bcfed6e6b81e2
Sagar Dhunna <sagardhunna17@gmail.com>
2024-12-25 16:34:10 -0800

    Made website look better on mobile, changed sizing to use percentages

--- Frontend/src/components/projects/Projects.jsx
+++ Frontend/src/components/projects/Projects.jsx
@@ -8,37 +8,31 @@ import Col from 'react-bootstrap/Col';
 const Projects = () => {
     return (
         <div className='projectCards'>
-            <div className='card-styling rounded'>
-                <h2 style={{ margin: '10px' }}>RemindMe</h2>
-                <Card style={{ width: '20rem', height: '20rem', padding: '0px' }}>
+            <div className='card-styling rounded projectCard'>
+                <h2 style={{ marginBottom: '8%' }}>Remind Me</h2>
+                <div className='container'>
                     <div className='project1Image'>
                         <Card.Img
                             variant="top"
                             src="/assets/AlarmImage@3x.png"
-                            style={{width: '100%', height: '100%'}}
+                            style={{ width: '100%', height: '100%' }}
                         />
                     </div>
-                    <Card.Body>
-                        <Card.Text>
-                            {/*Can put card text here if we want to*/}
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
+                </div>
                 <Button className='goSomewhereButton border border-dark border-2' variant='light'>Go somewhere</Button>
             </div>
 
-            <div className='card-styling rounded'>
-                <h2 style={{ margin: '10px' }}>PROJECTNAME</h2>
-                <Card style={{ width: '20rem', height: '20rem', padding: '0px' }}>
-                    <div className='project1Image' style={{width: '105%', height: '105%', alignSelf: 'center'}}>
-                        <Card.Img variant="top" src="holder.js/400px400" />
+            <div className='card-styling rounded projectCard'>
+                <h2 style={{ marginBottom: '8%' }}>PROJECTNAME</h2>
+                <div className='container'>
+                    <div className='project1Image'>
+                        <Card.Img
+                            variant="top"
+
+                            style={{ width: '100%', height: '100%' }}
+                        />
                     </div>
-                    <Card.Body>
-                        <Card.Text>
-                            {/*Can put card text here if we want to*/}
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
+                </div>
                 <Button className='goSomewhereButton border border-dark border-2' variant='light'>Go somewhere</Button>
             </div>
         </div>
