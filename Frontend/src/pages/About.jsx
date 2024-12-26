65e8b5ed56be33d983fb213b0d485a7aaadedd93
Sagar Dhunna <sagardhunna17@gmail.com>
2024-12-26 00:04:19 -0800

    about info section changes size based on how many words are inputted

--- Frontend/src/pages/About.jsx
+++ Frontend/src/pages/About.jsx
@@ -6,8 +6,10 @@ import AboutMe from "../components/aboutme/AboutMe"
 function About() {
     return (
       <div className="about-container">
-        <h1>About Me</h1>
-        <AboutMe />
+        <div className='about-section'>
+          <h1>About Me</h1>
+          <AboutMe />
+        </div>
         <h1>Education History</h1>
         <EducationCards />
       </div>
