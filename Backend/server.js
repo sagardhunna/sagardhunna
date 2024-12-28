f6a825420485cfecefdeca430cd71284b9f2d063
Sagar Dhunna <sagardhunna17@gmail.com>
2024-12-27 23:09:32 -0800

    work on backend to fix deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const { createClient } = require("pexels");
 
 app.use(cors());
